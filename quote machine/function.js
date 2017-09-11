bb6f37853c346a60353eb6faaa82f37aca2908bc
LAKSHITgoel <glakshit0@gmail.com>
2017-09-11 19:55:02 +0530

    first commit

---
+++ quote machine/function.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    curQuote='';
+    curAuthor='';
+   $("#new_quote").on("click",function(){
+       $.ajax({
+       headers: {
+               "X-Mashape-Key": "jKdq5Vn0lsmshVNnjhHMo9RQhcMRp1VLHGHjsnz8oqiqkYQ9jK",
+               Accept: "application/json",
+               "Content-Type": "application/x-www-form-urlencoded"
+               },    
+       url:"https://andruxnet-random-famous-quotes.p.mashape.com/?cat=", 
+       success: function(r)
+               {
+               if(typeof r == 'string'){
+                   r=JSON.parse(r);
+                   
+               }
+               curAuthor=r.author;
+                   curQuote=r.quote;
+           $("#quote").html("<p>"+r.quote+"</p>" + "<br><p>— " + r.author + "</p>");
+           } 
+       });
+   });
+   $("#tweet-quote").on("click",function(){
+       $("#tweet-quote").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + curQuote + '" ' +"-"+curAuthor));
+   });
+});
+
+
